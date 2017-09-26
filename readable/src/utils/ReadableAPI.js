ac9362903b2c72f7de4e91c839918f16dcfec0da
Nikhil Narula <nikhil.narula85@gmail.com>
2017-09-26 17:45:09 -0500

    Refactor the complete folder structure to match the project requirements

--- readable/src/utils/ReadableAPI.js
+++ readable/src/utils/ReadableAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CATEGORIES_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CATEGORIES_API_URL || 'http://localhost:3001'
 
 let token = localStorage.token
 
